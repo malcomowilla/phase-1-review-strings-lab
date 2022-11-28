4542045c0631f2d71d25061c2d2ba0499a0707b1
malcomowilla <owillamalcom@gmail.com>
2022-11-28 08:43:33 +0300

    completed assighnment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = 'welcome, ${currentUser.slice(0, 1)}!';
+
+
+
+
+
+
+
+
+
+
+
+ 
