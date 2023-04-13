eaec192249e31132bf48092b156b34793e6de966
malcomowilla <owillamalcom@gmail.com>
2023-04-13 12:35:50 +0300

    solved again

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 // Write your code in this file!
-const currentUser = 'malcom';
+// const currentUser = 'malcom';
 
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-
-
+// const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 
 
-const excitedWelcomeMessage = welcomeMessage.toUpperCase(); 
-
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
 
 
+// const excitedWelcomeMessage = welcomeMessage.toUpperCase(); 
 
+// const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
+
+const currentUser = 'Malcom Owilla';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`
+
+const name = 'Edsger Dijkstra' .slice(0, 7)
+console.log(name)
+
+const numbers = [1, 2, 3, 2, 2, 6, 4, 45, 4, 5, 45, 5, 5]
+console.log(numbers.slice(7))
+
+
+// const myString = 'template literal';
+
+// const myNumber = 10;
+
+// const myBoolean = false;
+
+// const myMessage =`Saying that interpolation with ${myString}s is better than concatenation ${90 + myNumber}% of the time is simply ${myBoolean}. But is pretty cool!
+// \n Beware that new lines inside of a ${myString} will be prserved as new lines in the resulting ${typeof myString}! `;
+
+// console.log(myMessage)
 
 
 
